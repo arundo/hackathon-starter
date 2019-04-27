f65b4afc13cb37e9eb1d63348d8d8c36f3ebb357
ykrueng <yasirah.krueng@gmail.com>
2019-04-27 14:03:59 -0500

    refactoring

--- src/server/api.js
+++ src/server/api.js
@@ -13,6 +13,31 @@ const getKeyFromDate = date => new Date(
   date.getDate()
 ).toISOString()
 
+const parseTime = timeStr => {
+  return Number(timeStr.split(' ')[1].split(':')[0])
+}
+
+const aggregate = data => {
+  const sum = data.reduce((acc, item) => ({
+    current_temp: acc.current_temp + item.current_temp,
+    target_temp: acc.target_temp + item.target_temp,
+    outside_temp: acc.outside_temp + item.outside_temp
+  }), {
+      current_temp: 0,
+      target_temp: 0,
+      outside_temp: 0,
+  })
+
+  return ({
+    time: parseTime(data[0].time),
+    current_temp: sum.current_temp/data.length,
+    target_temp: sum.target_temp/data.length,
+    outside_temp: sum.outside_temp/data.length,
+  })
+}
+
+app.get('/')
+
 app.get('/daily', async (req, res) => {
   const { date, interval, csv } = req.query
 
@@ -26,28 +51,6 @@ app.get('/daily', async (req, res) => {
 
   let time = Number(interval)
 
-  const parseTime = timeStr => {
-    return Number(timeStr.split(' ')[1].split(':')[0])
-  }
-
-  const aggregate = data => {
-    const sum = data.reduce((acc, item) => ({
-      current_temp: acc.current_temp + item.current_temp,
-      target_temp: acc.target_temp + item.target_temp,
-      outside_temp: acc.outside_temp + item.outside_temp
-    }), {
-        current_temp: 0,
-        target_temp: 0,
-        outside_temp: 0,
-    })
-
-    return ({
-      time: parseTime(data[0].time),
-      current_temp: sum.current_temp/data.length,
-      target_temp: sum.target_temp/data.length,
-      outside_temp: sum.outside_temp/data.length,
-    })
-  }
   const groupData = response.reduce((acc, res) => {
     if (parseTime(res.time) < time) {
       acc[acc.length-1].push(res)
