683ccab57ffe1b14759ce3a8d680baace8fbdc1f
ch0mper <hannahchang17@gmail.com>
2019-04-27 14:11:35 -0500

    added custom route to filter data for a specific date

--- src/rails-server/app/controllers/temperatures_controller.rb
+++ src/rails-server/app/controllers/temperatures_controller.rb
@@ -9,4 +9,10 @@ class TemperaturesController < ApplicationController
     @temperature = Temperature.find(params[:id])
     render json: @temperature
   end
+
+  def filter_one_date
+   @temperatures = Temperature.all
+   temperature = @temperatures.select{ |temp| temp.time.include?(params[:date]) }
+   render json: temperature
+  end
 end
--- src/rails-server/config/routes.rb
+++ src/rails-server/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'temperatures/filter_one_date/:date', to: 'temperatures#filter_one_date'
   resources :temperatures
 end
