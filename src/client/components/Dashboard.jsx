da176423f2169da1b06a4a5236d828d0cca504c6
ykrueng <yasirah.krueng@gmail.com>
2019-04-28 18:00:41 -0500

    add functionality to move to next/prev day/week

--- src/client/components/Dashboard.jsx
+++ src/client/components/Dashboard.jsx
@@ -1,69 +1,88 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import moment from 'moment'
 import {
   XYPlot,
   XAxis,
   YAxis,
   HorizontalGridLines,
   AreaSeries,
+  LineSeries,
   DiscreteColorLegendItem
 } from 'react-vis'
-import { async } from 'rxjs/internal/scheduler/async';
+// import { async } from 'rxjs/internal/scheduler/async';
 
 const Dashboard = () => {
   const [data, setData] = useState([])
-  const [date, setDate] = useState('2018/11/01')
-  const [intv, setintv] = useState('2')
   const [dateText, setDateText] = useState('')
   const [intvText, setIntvText] = useState('')
+  const [date, setDate] = useState('2018-11-01')
+  const [intv, setIntv] = useState(2)
+
+  const refDate = React.createRef
 
   useEffect(() => {
-    const getData = async() => {
-      const res = await axios.get(`http://localhost:3000/api/daily?date=${date}&interval=${intv}`)
-      setData(res.data)
-    }
+    date
     getData()
-  }, [])
+  }, [date])
 
   const getData = async () => {
-    const res = await axios.get(`http://localhost:3000/api/daily?date=${dateText || date}&interval=${intvText || intv}`)
-    setData(res.data)
+    try{
+      const res = await axios.get(`http://localhost:3000/api/daily?date=${moment(date).format('YYYY/MM/DD')}&interval=${intv}`)
+      setData(res.data)
+    } catch (err) {
+      setData([])
+    }
   }
-
-  if (data.length === 0) return <div>Loading</div>
 
   return (
     <div>
-      <XYPlot width={1000} height={400}>
-        <AreaSeries data={data.map(item => (
-          { x: item.time, y: item.outside_temp }))}
-          opacity={0.25}
-          color="#DB463B"
-        />
-        <HorizontalGridLines />
-        <AreaSeries data={data.map(item => (
-          { x: item.time, y: item.current_temp }))}
-          opacity={0.25}
-          color="#DA3F6D"
-        />
-        <AreaSeries data={data.map(item => (
-          { x: item.time, y: item.target_temp }))}
-          opacity={0.25}
-          color="#7548C9"
-        />
-        <XAxis />
-        <YAxis />
-      </XYPlot>
+      {
+        data.length === 0 && <div style={{width: 1000, height: 400}}> No Data </div>
+      }
+      {
+        data.length !== 0 &&
+        <XYPlot width={1000} height={400}>
+          <LineSeries data={data.map(item => (
+            { x: item.time, y: item.outside_temp }))}
+            opacity={0.25}
+            color="#DB463B"
+          />
+          <HorizontalGridLines />
+          <LineSeries data={data.map(item => (
+            { x: item.time, y: item.current_temp }))}
+            opacity={0.25}
+            color="#DA3F6D"
+          />
+          <LineSeries data={data.map(item => (
+            { x: item.time, y: item.target_temp }))}
+            opacity={0.25}
+            color="#7548C9"
+          />
+          <XAxis />
+          <YAxis />
+        </XYPlot>
+      }
+      <button onClick={() => { setDate(moment(date).add(-7, 'days').format('YYYY-MM-DD')) }} >
+        {'<<'}
+      </button>
+      <button onClick={() => { setDate(moment(date).add(-1, 'days').format('YYYY-MM-DD')) }} >
+        {'<'}
+      </button>
       <input
         type="date"
-        value={dateText}
-        onChange={e => setDateText(e.target.value)}
+        value={date}
+        onChange={e => {
+          setDate(e.target.value)
+          getData()
+        }}
       />
-      <input
-        value={intvText}
-        onChange={e => setIntvText(e.target.value)}
-      />
-      <button onClick={getData}>Change</button>
+      <button onClick={() => { setDate(moment(date).add(1, 'days').format('YYYY-MM-DD')) }} >
+        >
+      </button>
+      <button onClick={() => { setDate(moment(date).add(7, 'days').format('YYYY-MM-DD')) }} >
+        >>
+      </button>
     </div>
   )
 }
